53d73933dfffd30befb2059bbf7be273fce0cae2
Kavitha Krishnan <krishnanka@vmware.com>
2022-03-08 14:58:51 -0500

    Add a new validating webhook

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -43,6 +43,11 @@ var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
 	v1alpha1.SchemeGroupVersion.WithKind("AddressableService"): &v1alpha1.AddressableService{},
 }
 
+var deploymentTypes = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
+	// List the types to validate.
+	v1alpha1.SchemeGroupVersion.WithKind("SimpleDeployment"): &v1alpha1.SimpleDeployment{},
+}
+
 var callbacks = map[schema.GroupVersionKind]validation.Callback{}
 
 func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
@@ -96,6 +101,33 @@ func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher
 	)
 }
 
+func NewDeploymentValidationAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return validation.NewAdmissionController(ctx,
+
+		// Name of the resource webhook.
+		fmt.Sprintf("validation.webhook.deployment.%s.knative.dev", system.Namespace()),
+
+		// The path on which to serve the webhook.
+		"/deployment-validation",
+
+		// The resources to validate.
+		deploymentTypes,
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		func(ctx context.Context) context.Context {
+			// Here is where you would infuse the context with state
+			// (e.g. attach a store with configmap data)
+			return ctx
+		},
+
+		// Whether to disallow unknown fields.
+		true,
+
+		// Extra validating callbacks to be applied to resources.
+		callbacks,
+	)
+}
+
 func NewConfigValidationController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
 	return configmaps.NewAdmissionController(ctx,
 
@@ -125,5 +157,6 @@ func main() {
 		NewDefaultingAdmissionController,
 		NewValidationAdmissionController,
 		NewConfigValidationController,
+		NewDeploymentValidationAdmissionController,
 	)
 }
